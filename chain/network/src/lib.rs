a11c14d890c4b531aec0d7b4dadb2e45a3296d7e
Piotr Mikulski <piotr@near.org>
2021-12-03 06:13:09 -0800

    Add comments

--- chain/network/src/lib.rs
+++ chain/network/src/lib.rs
@@ -1,5 +1,6 @@
 pub use crate::peer_manager::peer_manager_actor::PeerManagerActor;
 pub use crate::peer_manager::peer_store::iter_peers_from_store;
+/// For benchmarks only
 pub use crate::routing::routing_table_actor::RoutingTableActor;
 #[cfg(feature = "test_features")]
 pub use crate::routing::routing_table_actor::{RoutingTableMessages, RoutingTableMessagesResponse};
