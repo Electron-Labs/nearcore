648524c643475831cd58ab0969471585084510c3
suniljalandhra <jalandhrasunil99@gmail.com>
2021-12-21 13:19:49 +0530

    CVF-9 resolved  it is convention type error

--- runtime/near-vm-logic/src/alt_bn128.rs
+++ runtime/near-vm-logic/src/alt_bn128.rs
@@ -52,8 +52,8 @@ pub fn alt_bn128_g1_multiexp_sublinear_complexity_estimate(n_bytes: u64, discoun
         0
     } else {
         // set linear complexity growth for n > 32768
-        let l = std::cmp::min(ilog2(n), 15);
-        (n * (l + 3) + (1 << (1 + l))) * C / ((l + 4) * (l + 5))
+        let ell = std::cmp::min(ilog2(n), 15);
+        (n * (ell + 3) + (1 << (1 + ell))) * C / ((ell + 4) * (ell + 5))
     };
     res.saturating_sub(B*n+discount)
 }
